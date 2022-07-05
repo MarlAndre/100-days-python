588adb6e2e5903044bcdfd1a6beea4f9fcf14427
Marlene Andre <andre.marlene1990@gmail.com>
2022-07-05 17:58:20 -0400

    simplefied version of the Coffee Machine chalenge using classes

--- day_16/oop-coffee-machine-start/main.py
+++ day_16/oop-coffee-machine-start/main.py
@@ -1,3 +1,31 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+
+is_on = True
+
+
+while is_on:
+    order = menu.get_items()
+    choice = input(f"What would you like? {order}: ")
+    if choice == "off":
+        is_on = False
+# TODO 1 Print report
+    elif choice == "report":
+        coffee_report = coffee_maker.report()
+        profit_report = money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        # TODO 2 Check resources
+        if coffee_maker.is_resource_sufficient(drink):
+            # TODO 3 Process coins
+            # TODO 4 Check transaction
+            if money_machine.make_payment(drink.cost):
+                # TODO 5 Make coffee
+                serve_drink = coffee_maker.make_coffee(drink)
+
