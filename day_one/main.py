319fe80eab9c7e192592538689238832b21c437e
Marlène <andre.marlene1990@gmail.com>
2022-07-08 15:20:59 -0400

    Delete day_one directory

--- day_one/main.py
+++
@@ -1,9 +0,0 @@
-#1. Create a greeting for your program.
-print('Welcome to the band generator program!')
-
-#2. Ask the user for the city that they grew up in.
-city = input("In which city did you grew up?: ")
-#3. Ask the user for the name of a pet.
-pet = input("What is the name of your pet?: ")
-#4. Combine the name of their city and pet and show them their band name.
-print(f"You should call your band: {city} {pet}")
