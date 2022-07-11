9210a1845a85db84bc94b9fa3d34106919dd1db6
Marlene Andre <andre.marlene1990@gmail.com>
2022-07-11 08:27:36 -0400

    snake can move up and down, left and right

--- day_20/snake-game/snake.py
+++ day_20/snake-game/snake.py
@@ -1,12 +1,16 @@
 from turtle import Turtle
 PACE = 20
-
+UP = 90
+RIGHT = 0
+DOWN = 270
+LEFT = 180
 
 class Snake:
 
     def __init__(self):
         self.snake_body = []
         self.create_body()
+        self.head = self.snake_body[0]
 
 # Create initial body
     def create_body(self):
@@ -24,6 +28,24 @@ class Snake:
             new_x = self.snake_body[seg_num - 1].xcor()
             new_y = self.snake_body[seg_num - 1].ycor()
             self.snake_body[seg_num].goto(new_x, new_y)
-        self.snake_body[0].forward(PACE)
+        self.head.forward(PACE)
+
+    def up(self):
+        # If snake is going down, he can't go up, so user have to use left or right key to move snake
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
 
 
